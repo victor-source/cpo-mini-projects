df8e0c15caba40ea3854eac9a0906222ac9887aa
pythonier <vicjohn2018@gmail.com>
2020-06-06 21:05:13 +0100

    Add files via upload

---
+++ inheritance.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Mother
+{
+ public:
+  Mother() {};
+  void sayHi() {
+   cout << "Hi";
+  }
+};
+
+class Daughter: public Mother
+{
+ public:
+  Daughter() {};
+};
+
+int main() {
+  Daughter d;
+  d.sayHi();
+}
+//Outputs "Hi"
---
+++ this.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class MyClass {
+ public:
+  MyClass(int a) : var(a)
+  { }
+  void pritInfo() {
+   cout << var<<endl;
+   cout << this->var<<endl;
+   cout << (*this).var<<endl; 
+  }
+ private:
+  int var;
+};
+int main()
+{
+MyClass obj(29);
+obj.pritInfo();
+}
